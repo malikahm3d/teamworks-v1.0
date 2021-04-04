bd4a689a5641123c5a39b3d245f38e7a4b0672a2
malikahm3d <44858076+malikahm3d@users.noreply.github.com>
2021-04-04 05:21:12 +0300

    other

--- resources/views/courses/posts/PostsInACourse.blade.php
+++ resources/views/courses/posts/PostsInACourse.blade.php
@@ -2,23 +2,8 @@
 
 @section('content')
 
-        <div class="row m-4 mb-5">
-            <div class="col-6 offset-3">
-                <h1 class="text-center">New Post!</h1>
-                <form action="{{ route('createPost', $course) }}" method="POST" novalidate class="needs-validation">
-                    @csrf
-                    <div class="mb-3">
-                        <label class="form-label" for="title">Title</label>
-                        <input class="form-control" type="text" name="title" id="title" placeholder="Enter Title" required>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label" for="body">body</label>
-                        <textarea class="form-control" type="text" name="body" id="body" placeholder="Enter Post Body" required></textarea>
-                    </div>
-                    <button class="btn btn-outline-primary mb-2">Submit</button>
-                </form>
-            </div>
-        </div>
+
+    <a href="{{route('post.crate')}}">Create Post!</a>
 
     @if($posts->count())
         @foreach($posts as $post)
@@ -27,26 +12,7 @@
     @else
         <p class="text-center text-danger text-md-center">There are no posts in this course</p>
         <p class="text-center link-primary"><a href="{{ route('showCourses') }}">You Can Enroll In Other Courses Now!</a></p>
+        <p class="text-center link-primary"><a href="{{ route('post.crate') }}">Create Post!</a></p>
     @endif
-
-<script>
-    // Example starter JavaScript for disabling form submissions if there are invalid fields
-    (function() {
-        'use strict';
-        window.addEventListener('load', function() {
-            //TODO: more advanced from checking with individual error messages
-            var forms = document.getElementsByClassName('needs-validation');
-            // Loop over them and prevent submission
-            var validation = Array.prototype.filter.call(forms, function(form) {
-                form.addEventListener('submit', function(event) {
-                    if (form.checkValidity() === false) {
-                        event.preventDefault();
-                        event.stopPropagation();
-                    }
-                    form.classList.add('was-validated');
-                }, false);
-            });
-        }, false);
-    })();
-</script>
+    
 @endsection
